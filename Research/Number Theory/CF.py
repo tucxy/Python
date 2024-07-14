db6038ee184abd441a63b51d1f0190be9cd5658f
Danny Banegas <baneg003@umn.edu>
2024-07-14 01:34:25 -0500

    new research

---
+++ Research/Number Theory/CF.py
@@ -0,0 +1,13 @@
+import contfrac as cf
+import math as m
+
+x = 415/93
+y = m.sqrt(5)
+print(list(cf.continued_fraction(x)))
+print(list(cf.continued_fraction(y)))
+
+
+
+
+
+
--- Side Projects/fib.py
+++ Side Projects/fib.py
@@ -1,23 +1,27 @@
-import pandas as pd
 import math
 
 n = 150
 L = [1, 3]  # Starting values for the Fibonacci sequence
 
-def fib(k, I):
+def fib(n, I):
     L = list(I)
-    while len(L) < k:  # Ensure we generate exactly k numbers
-        L.append(L[-2] + L[-1])
-    return L
-
+    L.append((L[0]+L[1]) % n)
+    current = [L[1],L[2]]
+    count = 1
+    while current != [L[0],L[1]]:
+        L.append((current[0]+current[1])%n)
+        current = [current[1],(current[0]+current[1])%n]
+        count += 1
+    return L      
 
 # Generate Fibonacci numbers
-fibonacci_sequence = fib(n, L)
+fibseq = fib(7, [0,1])
+print(fibseq)
 
-# Convert the list to a DataFrame and write to a CSV file
+''' Convert the list to a DataFrame and write to a CSV file
 df = pd.DataFrame(fibonacci_sequence, columns=['Fibonacci Number'])
 df.to_csv('fibonacci_sequence.csv', index=False)
 
 print("Fibonacci sequence has been written to fibonacci_sequence.csv.")
-
+'''
 
