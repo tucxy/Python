165947706015d7802682b7da362e75109cb97d38
Danny Banegas <baneg003@umn.edu>
2024-07-17 15:33:16 -0500

    update

--- Research/Number Theory/CF.py
+++ Research/Number Theory/CF.py
@@ -1,13 +1,34 @@
-import contfrac as cf
-import math as m
-
-x = 415/93
-y = m.sqrt(5)
-print(list(cf.continued_fraction(x)))
-print(list(cf.continued_fraction(y)))
+import math
+from decimal import Decimal, getcontext
 
 
+getcontext().prec = 100
+
+def fincf(x, r, n):
+    x = Decimal(x)
+    r = Decimal(r)
+
+    X = [x]
+    A = [math.floor(x)]
+    term = 0
+
+    for i in range(n):
+        if X[i] == Decimal(math.floor(X[i])):
+            term = i
+            break
+        else:
+            xc = r / (X[i] - Decimal(math.floor(X[i])))
+            ac = math.floor(xc)
+
+            X.append(xc)
+            A.append(ac)
+    
+    return X, A, term
 
 
+x = Decimal(1.4142135623730950488016887242096980785696718753769480731766797379)
+r = Decimal(3) / Decimal(2)
+n = 100
 
-
+result = fincf(x, r, n)[1]
+print(result)
